90b9fead047ca70480f32ec37efe8d6f2acfcf0d
David Kirwan <dkirwan@redhat.com>
2016-12-19 10:43:05 +0000

    Updated the pir motion detection code to scale to polling multiple sensors - currently 4 configured in code.

---
+++ pir_motion_sensor/pir_motion_sensor.ino
@@ -0,0 +1,46 @@
+/**
+ * trashtalk - Interactive Rubbish Bin
+ * 
+ * A South East Makerspace project developed for the Waterford Imagine Festival 2016 
+ * Github: https://github.com/semkr/digistreets
+ * Wiki: https://wiki.southeastmakerspace.org/projects/digistreets/talking_rubbish/start
+ * Version: 0.1.0
+ */
+
+#define SENSOR_1  2
+#define SENSOR_2  3
+#define SENSOR_3  4
+#define SENSOR_4  5
+
+#define PIN_OUT 8
+
+
+/**
+ * Read the value from each sensor each second
+ * Print output to the serial 0 low, 1 high, one value per sensor
+ * eg: 0,0,1,0\n
+ */
+void read_sensors(){
+  int val1 = digitalRead(SENSOR_1);
+  int val2 = digitalRead(SENSOR_2);
+  int val3 = digitalRead(SENSOR_3);
+  int val4 = digitalRead(SENSOR_4);
+  Serial.println(String(val1) + "," + String(val2) + "," + String(val3) + "," + String(val4));
+  delay(1000);
+}
+
+
+void setup(){
+  Serial.begin(9600);
+  
+  pinMode(SENSOR_1,  INPUT);
+  pinMode(SENSOR_2,  INPUT);
+  pinMode(SENSOR_3,  INPUT);
+  pinMode(SENSOR_4,  INPUT);
+  
+  pinMode(PIN_OUT, OUTPUT);
+}
+
+void loop(){
+  read_sensors();
+}

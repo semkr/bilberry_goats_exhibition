13b279e82dfb5d44c5da48051e856bcc41a97937
David Kirwan <dkirwan@redhat.com>
2017-01-29 20:24:12 +0000

    - Cronjob and launcher script

---
+++ scripts/bilberry_goats_launcher.sh
@@ -0,0 +1,8 @@
+PATH=/home/sems/bin:/home/sems/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+echo $PATH
+
+killall java
+
+export DISPLAY=:1
+
+/home/sems/Desktop/bilberry_goats_exhibition/video_projection/application.linux64/video_projection
